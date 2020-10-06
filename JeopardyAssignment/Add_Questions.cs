48d045a6ad420b0051d70ca9d0f7c39e57c40103
Marcus Sjöholm <marcusasjoholm93@gmail.com>
2020-10-06 15:30:21 +0200

    Add project files.

---
+++ JeopardyAssignment/Add_Questions.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Jeopardy
+{
+    internal class Add_Questions
+    {
+        public static void File_Location() { }
+        public static List<string> AvailableSeasons { get; } = new List<string>();
+        public static void Add_Question()
+        {
+            AvailableSeasons.Add("season1");
+            AvailableSeasons.Add("season2");
+            AvailableSeasons.Add("season3");
+            AvailableSeasons.Add("season4");
+            AvailableSeasons.Add("season5");
+            AvailableSeasons.Add("season6");
+            AvailableSeasons.Add("season7");
+            AvailableSeasons.Add("season8");
+            AvailableSeasons.Add("season9");
+            AvailableSeasons.Add("season10");
+            AvailableSeasons.Add("season11");
+            AvailableSeasons.Add("season12");
+            AvailableSeasons.Add("season13");
+            AvailableSeasons.Add("season14");
+            AvailableSeasons.Add("season15");
+            AvailableSeasons.Add("season16");
+            AvailableSeasons.Add("season17");
+            AvailableSeasons.Add("season18");
+            AvailableSeasons.Add("season19");
+            AvailableSeasons.Add("season20");
+            AvailableSeasons.Add("season21");
+            AvailableSeasons.Add("season22");
+            AvailableSeasons.Add("season23");
+            AvailableSeasons.Add("season24");
+            AvailableSeasons.Add("season25");
+            AvailableSeasons.Add("season26");
+            AvailableSeasons.Add("season27");
+            AvailableSeasons.Add("season28");
+            AvailableSeasons.Add("season29");
+            AvailableSeasons.Add("season30");
+            AvailableSeasons.Add("season31");
+            AvailableSeasons.Add("season32");
+            AvailableSeasons.Add("season33");
+            AvailableSeasons.Add("season34");
+            AvailableSeasons.Add("season35");
+            AvailableSeasons.Add("season36");
+        }
+
+        public static void Show_Supported_Seasons()
+        {
+            Console.WriteLine("Seasons available:");
+            string AvailableString = string.Join(", ", AvailableSeasons.ToArray());
+            Console.WriteLine(AvailableString);
+        }
+
+        public static bool Supported(string Question)
+        {
+            bool success = false;
+            foreach (string question in AvailableSeasons)
+            {
+                if (Question.Contains(question))
+                {
+                    success = true;
+                }
+            }
+            return success;
+        }
+        public Dictionary<string, int> Text_In_File { get; set; }
+    }
+}
+
