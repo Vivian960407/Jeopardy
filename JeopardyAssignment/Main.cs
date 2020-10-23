4f3d6505437ed5514449bc512ce4044dad36d6a1
Marcus Sjöholm <marcusasjoholm93@gmail.com>
2020-10-23 11:40:30 +0200

    Song

--- JeopardyAssignment/Main.cs
+++ JeopardyAssignment/Main.cs
@@ -4,7 +4,8 @@
     {
         public static void Main()
         {
-            //WelcomeMessage.Welcome();
+            WelcomeMessage.Welcome();
+            WelcomeMessage.Song();
             var game = new Game();
             game.Start();
 
--- JeopardyAssignment/Welcome.cs
+++ JeopardyAssignment/Welcome.cs
@@ -29,10 +29,62 @@ namespace JeopardyAssignment
             Console.WriteLine(@"| |              | || |              | || |              | || |              | |");
             Console.WriteLine(@"| '--------------' || '--------------' || '--------------' || '--------------' |");
             Console.WriteLine(@" '----------------'  '----------------'  '----------------'  '----------------' ");
-
-
-            Thread.Sleep(5000);
+            Console.Beep(784, 150);
+            Thread.Sleep(300);
+            Console.Beep(784, 150);
+            Thread.Sleep(300);
+            Console.Beep(932, 150);
+            Thread.Sleep(150);
+            Console.Beep(1047, 150);
+            Thread.Sleep(150);
+            Console.Beep(784, 150);
+            Thread.Sleep(300);
+            Console.Beep(784, 150);
+            Thread.Sleep(300);
+            Console.Beep(699, 150);
+            Thread.Sleep(150);
+            Console.Beep(740, 150);
+            Thread.Sleep(150);
+            Console.Beep(784, 150);
+            Thread.Sleep(300);
+            Console.Beep(784, 150);
+            Thread.Sleep(300);
+            Console.Beep(932, 150);
+            Thread.Sleep(150);
+            Console.Beep(1047, 150);
+            Thread.Sleep(150);
+            Console.Beep(784, 150);
+            Thread.Sleep(300);
+            Console.Beep(784, 150);
+            Thread.Sleep(300);
+            Console.Beep(699, 150);
+            Thread.Sleep(150);
+            Console.Beep(740, 150);
+            Thread.Sleep(150);
+            Console.Beep(932, 150);
+            Console.Beep(784, 150);
+            Console.Beep(587, 1200);
+            Thread.Sleep(75);
+            Console.Beep(932, 150);
+            Console.Beep(784, 150);
+            Console.Beep(587, 1200);
+            Thread.Sleep(75);
+            Console.Beep(932, 150);
+            Console.Beep(784, 150);
+            Console.Beep(587, 1200);
+            Thread.Sleep(75);
+            Console.Beep(932, 150);
+            Console.Beep(784, 150);
+            Console.Beep(523, 1200);
+            Thread.Sleep(150);
+            Console.Beep(466, 150);
+            Console.Beep(523, 150);
             Console.Clear();
         }
+        public static void Song()
+        {
+           
+        }
     }
+
 }
